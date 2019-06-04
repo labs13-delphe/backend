d7be97f62c615dbb18a67538dbb531997be671b4
AmarachiOE <ao.ejiawoko@gmail.com>
2019-06-04 14:54:50 -0700

    Added users and conversations tables. Revised questions, answers, expert_topics tables.

---
+++ data/migrations/20190604134513_users.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+
+    tbl.string("first_name", 128).notNullable();
+    tbl.string("last_name", 128).notNullable();
+    tbl.string("username", 128).notNullable();
+    tbl.string("password", 128).notNullable();
+    tbl.string("email", 128).notNullable();
+    tbl.text("bio");
+    tbl.string("image_url");
+    tbl.string("user_type").defaultTo("asker");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
