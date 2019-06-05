26e56a8f6f93194136c6e5ee42aef49080f39630
Josh Akeman <joshakeman@gmail.com>
2019-06-05 14:57:46 -0400

    updated users-model and users-router to include all CRUD operations

--- api/users-router.js
+++ api/users-router.js
@@ -36,4 +36,61 @@ router.delete('/:id', async (req, res) => {
 	}
 });
 
+router.post('/', async (req, res) => {
+	const user = req.body;
+  
+	if (user.username) {
+	  try {
+		const inserted = await Users.add(user);
+		res.status(201).json(inserted);
+	  } catch (error) {
+		res
+		  .status(500)
+		  .json({ message: 'We ran into an error creating the user' });
+	  }
+	} else {
+	  res.status(400).json({ message: 'Please provide name of the track' });
+	}
+  });
+
+  router.put('/:id', async (req, res) => {
+	const changes = req.body;
+  
+	if (changes.username) {
+	  try {
+		const updated = await Users.update(req.params.id, changes);
+		if (updated) {
+		  res.status(200).json(updated);
+		} else {
+		  res.status(404).json({
+			message: 'That user does not exist',
+		  });
+		}
+	  } catch (error) {
+		res
+		  .status(500)
+		  .json({ message: 'We ran into an error updating the user' });
+	  }
+	} else {
+	  res.status(400).json({
+		message: 'Please provide the name of the user',
+	  });
+	}
+  });
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const removed = await Users.remove(req.params.id);
+		if (removed) {
+			res.status(204).json({ success: 'User removed' });
+		} else {
+			res
+				.status(404)
+				.json({ message: 'The User with the specified ID does not exist.' });
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
