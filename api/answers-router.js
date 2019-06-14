1d9e3c6c8d856d4da4b25d0b630898cc8c1ca021
AmarachiOE <ao.ejiawoko@gmail.com>
2019-06-14 08:50:36 -0700

    Fixed endpoint for get answer by Id

--- api/answers-router.js
+++ api/answers-router.js
@@ -12,8 +12,29 @@ router.get("/", (req, res) => {
     });
 });
 
-//Get Ansewers by user
+// Get Answer by ID
 router.get("/:id", (req, res) => {
+  const answerId = req.params.id;
+  Answers.findById(answerId)
+    .then(answer => {
+      // if a question with an ID field exists (since an empty answer array was being returned even if 'question' didn't exist)
+      if (answer.id) {
+        res.status(200).json(answer);
+      } else {
+        res
+          .status(404)
+          .json({ error: "Whoops. This answer could not be found." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "Uh oh. This answer's information could not be retrieved."
+      });
+    });
+});
+
+//Get Ansewers by user
+router.get("/users/:id", (req, res) => {
   console.log(req.body)
   Answers.findUserAnswers(req.params.id)
     .then(answers => {
